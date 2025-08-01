cc36ae5b5e9577f7cd9516a8c5314b3c9bfbbf5c
witchx44@gmail.com <witchx44@gmail.com>
2025-08-01 10:41:11 +0530

    Changed vibration effect to click

--- app/src/main/java/com/bhashana/virtualback/FloatingBackService.kt
+++ app/src/main/java/com/bhashana/virtualback/FloatingBackService.kt
@@ -18,12 +18,11 @@ class FloatingBackService : AccessibilityService() {
         disableSelf()
     }
 
-
     @RequiresPermission(Manifest.permission.VIBRATE)
     private fun vibrate() {
         val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            v.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK))
+            v.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
         } else {
             // fallback for older devices
             v.vibrate(50)
